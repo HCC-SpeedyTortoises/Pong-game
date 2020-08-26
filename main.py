05ad859cf5862cefda8857b2235fc386d6b8d43b
Em4444 <69920558+Em4444@users.noreply.github.com>
2020-08-26 15:14:41 -0400

    Special Feature
    Collect "coins" (circles)

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ screen.setup(totalX+50,totalY+50)
 #print ('Screen size:', screen.screensize())
 screen.bgcolor(' light green')
 
-# Turtle 0 Creation => Draws boundaries for the Ping Pong Game
+# Turtle 0 Creation => Draws boundaries for the Ping Pong Game and draws circles (which give extra points)
 t0 = Turtle()
 t0.hideturtle()
 t0.color('black'); t0.speed(50)
@@ -37,6 +37,22 @@ t0.penup(); t0.goto(-150,totalY//2+10); t0.write("PLAYER 1 : ", True, align = "l
 t0.penup(); t0.goto(-totalX//2,-totalY//2); t0.pendown(); t0.goto(-totalX//2,totalY//2); t0.goto(totalX//2,totalY//2); t0.goto(totalX//2,-totalY//2); t0.goto(-totalX//2,-totalY//2); 
 t0.penup(); #t0.goto(-180,200); t0.pendown();t0.goto(-180, -200); t0.penup()
 
+# draw a circle
+t0.goto(100,100)
+t0.pendown()
+t0.color("yellow")
+t0.fillcolor("yellow")
+t0.begin_fill()
+t0.circle(10)
+t0.end_fill()
+
+# test
+t0.penup
+t0.goto(50,50)
+t0.pendown()
+for i in range(4):
+  t0.fd(100)
+  t0.left(90)
 
 # Turtle 1 Creation => Acts as a slider controlled by 'Up' & 'Down' key
 paddle1 = Turtle()
@@ -179,6 +195,14 @@ def was_right_paddle_hit():
     return (ballY >= paddle1_bottom 
         and ballY <= paddle1_top 
         and ballX >= paddle1_left - ball_radius)
+  
+last_paddle_hit = 0
+
+# number of times each player pushed the ball into the edge of a collectable object
+times_p1_hit_edge = 0
+times_p2_hit_edge = 0
+
+ball_edge_status = "no"
 
 def move_ball():
   try:
@@ -189,14 +213,31 @@ def move_ball():
     global collision
     global start
     global player1_score, player2_score
-
-    #print(ballX, ballY)
-    ball.goto(ballX, ballY)
+    global last_paddle_hit
+    global times_p1_hit_edge
+    global times_p2_hit_edge
+    global ball_edge_status
 
     right_wall = totalX//2 - ball_radius
     left_wall = - totalX//2 + ball_radius
     top_wall = totalY//2 - ball_radius
     bottom_wall = - totalY//2 + ball_radius
+
+    ball.goto(ballX, ballY)
+
+    # Check if the ball is on the edge of a collectable object
+    if 50 < ball.ycor() < 150 and 49.9 <= ball.xcor() <= 50.9: 
+      # x is approx 50
+      ball_edge_status = "yes"
+    elif 50 < ball.ycor() < 150 and 149.9 <= ball.xcor() <=  150.9: 
+      # x is approx 150
+      ball_edge_status = "yes"
+    elif 50 < ball.xcor() < 150 and 49.9 <= ball.ycor() <= 50.9: 
+      # y is approx 50
+      ball_edge_status = "yes"
+    elif 50 < ball.xcor() < 150 and 149.9 <= ball.ycor() <=  150.9: 
+      # y is approx 150
+      ball_edge_status = "yes"
 
     # bounce off walls by changing direction of ball
     if ballY > top_wall:
@@ -208,12 +249,14 @@ def move_ball():
     if was_left_paddle_hit():
       print("left paddle was hit, bouncing")
       dX = -dX
+      last_paddle_hit = 1
 
     # hit on the paddle and bounce off by changing direction 
     if was_right_paddle_hit():
       print("right paddle was hit, bouncing")
       dX = -dX
-            
+      last_paddle_hit = 2   
+    
     # if hit left wall, restart and give player 2 a point
     if ballX < left_wall:
             start = 0
@@ -238,9 +281,26 @@ def move_ball():
         ballX = 0
         ballY = 0
     
+# if the ball is on the edge
+    if ball_edge_status == "yes":
+      if last_paddle_hit == 1:
+        times_p1_hit_edge += 1
+      elif last_paddle_hit == 2:
+        times_p2_hit_edge += 1
+      # if times_p1_hit_edge is even
+      if times_p1_hit_edge % 2 == 0:
+        player1_score += 1
+        p1_score_turtle.clear()
+        p1_score_turtle.write(player1_score, align = "left")
+      # if times_p2_hit_edge is even
+      elif times_p2_hit_edge % 2 == 0:
+        player2_score += 1
+        p2_score_turtle.clear() 
+        p2_score_turtle.write(player2_score, align = "left")
+
     # move the ball with updates every milisecond
     screen.ontimer(move_ball,1)
-    
+
   except Exception as e:
     print("ERROR in move_ball()!")
     print(e)
@@ -249,4 +309,5 @@ move_ball()
 
 screen.listen()
 screen.mainloop()
+
 
