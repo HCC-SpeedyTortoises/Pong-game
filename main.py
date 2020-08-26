0ba49beac696f11aa796cfafd5f98602c83a61f1
Cait Harrigan <cait.harrigan@mail.utoronto.ca>
2020-08-26 15:39:35 -0400

    Tanvi's rules added at top

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ dY = random() +1 #randint(1,2)
 
 # Screen initia
 screen = Screen()                        
-screen.setup(totalX+50,totalY+50)
+screen.setup(totalX+50,totalY+350)
 #screen.screensize(totalX,totalY)
 #screen.title("Ping Pong Game!")
 #print ('Screen size:', screen.screensize())
@@ -34,6 +34,10 @@ t0.color('black'); t0.speed(50)
 t0.penup(); t0.goto(0,totalY//2+ball_radius); t0.write("Press 'Space' to start", True, align="center")
 t0.penup(); t0.goto(150,totalY//2+10); t0.write("PLAYER 2 : ", True, align = "right")
 t0.penup(); t0.goto(-150,totalY//2+10); t0.write("PLAYER 1 : ", True, align = "left")
+t0.penup(); t0.goto(-30,totalY-100); t0.write("----= RULES =----", True, align = "left")
+t0.penup(); t0.goto(70,totalY-120); t0.write("- Player1 control keys are S,W ", True, align = "right")
+t0.penup(); t0.goto(97,totalY-130); t0.write("- Player2 control keys are arrow keys", True, align = "right")
+
 t0.penup(); t0.goto(-totalX//2,-totalY//2); t0.pendown(); t0.goto(-totalX//2,totalY//2); t0.goto(totalX//2,totalY//2); t0.goto(totalX//2,-totalY//2); t0.goto(-totalX//2,-totalY//2); 
 t0.penup(); #t0.goto(-180,200); t0.pendown();t0.goto(-180, -200); t0.penup()
 
@@ -47,12 +51,19 @@ t0.circle(10)
 t0.end_fill()
 
 # test
-t0.penup
+t0.penup()
 t0.goto(50,50)
 t0.pendown()
 for i in range(4):
   t0.fd(100)
   t0.left(90)
+
+# create the win turtle (writes which player won)
+win_turtle = Turtle()
+win_turtle.hideturtle()
+win_turtle.penup()
+win_turtle.goto(0,100)
+win_turtle.pendown()
 
 # Turtle 1 Creation => Acts as a slider controlled by 'Up' & 'Down' key
 paddle1 = Turtle()
@@ -103,6 +114,7 @@ def paddle2_down():
 def start_game():
     global start
     start = 1
+    win_turtle.clear(0)
 
 screen.onkey(paddle2_up, 'Up')
 screen.onkey(paddle2_down, 'Down')
@@ -122,12 +134,6 @@ p2_score_turtle.hideturtle()
 p2_score_turtle.penup()
 p2_score_turtle.goto(155,210)
 p2_score_turtle.pendown()
-
-
-def start_game():
-    global start
-    start = 1
-    print(ballX,ballY)
 
 def pause_game():
     global pause
@@ -195,8 +201,14 @@ def was_right_paddle_hit():
     return (ballY >= paddle1_bottom 
         and ballY <= paddle1_top 
         and ballX >= paddle1_left - ball_radius)
-
+  
 last_paddle_hit = 0
+
+# number of times each player pushed the ball into the edge of a collectable object
+times_p1_hit_edge = 0
+times_p2_hit_edge = 0
+
+ball_edge_status = "no"
 
 def move_ball():
   try:
@@ -208,6 +220,9 @@ def move_ball():
     global start
     global player1_score, player2_score
     global last_paddle_hit
+    global times_p1_hit_edge
+    global times_p2_hit_edge
+    global ball_edge_status
 
     right_wall = totalX//2 - ball_radius
     left_wall = - totalX//2 + ball_radius
@@ -215,6 +230,20 @@ def move_ball():
     bottom_wall = - totalY//2 + ball_radius
 
     ball.goto(ballX, ballY)
+
+    # Check if the ball is on the edge of a collectable object
+    if 50 < ball.ycor() < 150 and 49.9 <= ball.xcor() <= 50.9: 
+      # x is approx 50
+      ball_edge_status = "yes"
+    elif 50 < ball.ycor() < 150 and 149.9 <= ball.xcor() <=  150.9: 
+      # x is approx 150
+      ball_edge_status = "yes"
+    elif 50 < ball.xcor() < 150 and 49.9 <= ball.ycor() <= 50.9: 
+      # y is approx 50
+      ball_edge_status = "yes"
+    elif 50 < ball.xcor() < 150 and 149.9 <= ball.ycor() <=  150.9: 
+      # y is approx 150
+      ball_edge_status = "yes"
 
     # bounce off walls by changing direction of ball
     if ballY > top_wall:
@@ -257,20 +286,26 @@ def move_ball():
     else: 
         ballX = 0
         ballY = 0
-
-    if 50 < ball.xcor() < 150 and 50 < ball.ycor() < 150:
+    
+# if the ball is on the edge
+    if ball_edge_status == "yes":
       if last_paddle_hit == 1:
+        times_p1_hit_edge += 1
+      elif last_paddle_hit == 2:
+        times_p2_hit_edge += 1
+      # if times_p1_hit_edge is even
+      if times_p1_hit_edge % 2 == 0:
         player1_score += 1
         p1_score_turtle.clear()
         p1_score_turtle.write(player1_score, align = "left")
-      elif last_paddle_hit == 2:
+      # if times_p2_hit_edge is even
+      elif times_p2_hit_edge % 2 == 0:
         player2_score += 1
-        p2_score_turtle.clear()
+        p2_score_turtle.clear() 
         p2_score_turtle.write(player2_score, align = "left")
 
     # move the ball with updates every milisecond
     screen.ontimer(move_ball,1)
-
 
   except Exception as e:
     print("ERROR in move_ball()!")
